ed9aef5fae73478ea22e49410209403e1ce3c6e2
Alex Nestorov <nestorov.alexander@gmail.com>
2017-01-21 16:11:53 +0200

    add Ninject IoC Container

---
+++ SalaryCalculatorApp/SalaryCalculator.Data.Services/Contracts/IUserService.cs
@@ -0,0 +1,24 @@
+﻿using SalaryCalculator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Data.Services.Contracts
+{
+    public interface IUserService
+    {
+        IQueryable<User> GetTop(int count);
+
+        IQueryable<User> GetAll();
+
+        User GetById(int id);
+
+        void UpdateById(int id, User updateUser);
+
+        void DeleteById(int id);
+
+        void Create(User User);
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.Data.Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using SalaryCalculator.Data.Contracts;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Data.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> users;
+
+        public UserService(IRepository<User> users)
+        {
+            this.users = users;
+        }
+
+        public void Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> GetTop(int count)
+        {
+            return this.users.All.Take(count);
+        }
+
+        public void UpdateById(int id, User updateUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
