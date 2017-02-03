7a012f39b4296452e733cc13f60af169c0921d39
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-03 20:45:11 +0200

    add more EmployeeService tests

--- SalaryCalculatorApp/SalaryCalculator.Tests/Data.Services/EmployeeServicesTests.cs
+++ SalaryCalculatorApp/SalaryCalculator.Tests/Data.Services/EmployeeServicesTests.cs
@@ -6,6 +6,7 @@ using SalaryCalculator.Data.Models;
 using SalaryCalculator.Data.Contracts;
 using SalaryCalculator.Data.Services;
 using SalaryCalculator.Tests.Mocks;
+using System.Linq;
 
 namespace SalaryCalculator.Tests.Data.Services
 {
@@ -89,6 +90,65 @@ namespace SalaryCalculator.Tests.Data.Services
 
             emplService.GetAll();
             mockedRepository.Verify(r => r.All, Times.Once);
+        }
+
+        [Test]
+        public void GetById_ShouldInvokeOnlyOnce()
+        {
+            var mockedRepository = new Mock<IRepository<Employee>>();
+
+            var emplService = new EmployeeService(mockedRepository.Object);
+
+            var employee = new FakeEmployee();
+            employee.Id = 2;
+            emplService.Create(employee);
+
+            emplService.GetById(employee.Id);
+            mockedRepository.Verify(r => r.GetById(employee.Id), Times.Once);
+        }
+
+        [Test]
+        public void GetTop_ShouldInvokeOnlyOnce()
+        {
+            var mockedRepository = new Mock<IRepository<Employee>>();
+
+            var emplService = new EmployeeService(mockedRepository.Object);
+
+            Employee employee = new FakeEmployee();
+            Employee employee2 = new FakeEmployee();
+            Employee employee3 = new FakeEmployee();
+            employee.Id = 2;
+            employee2.Id = 3;
+            employee3.Id = 4;
+
+            emplService.Create(employee);
+            emplService.Create(employee2);
+            emplService.Create(employee3);
+
+            emplService.GetTop(3);
+            mockedRepository.Verify(r => r.All, Times.Once);
+        }
+
+        [Test]
+        public void GetTop_ShouldReturnInstanceOfIQueryable()
+        {
+            var mockedRepository = new Mock<IRepository<Employee>>();
+
+            var emplService = new EmployeeService(mockedRepository.Object);
+
+            Employee employee = new FakeEmployee();
+            Employee employee2 = new FakeEmployee();
+            Employee employee3 = new FakeEmployee();
+            employee.Id = 2;
+            employee2.Id = 3;
+            employee3.Id = 4;
+
+            emplService.Create(employee);
+            emplService.Create(employee2);
+            emplService.Create(employee3);
+
+            var query = emplService.GetTop(3);
+            Assert.IsInstanceOf<IQueryable<Employee>>(query);
         }
     }
 }
