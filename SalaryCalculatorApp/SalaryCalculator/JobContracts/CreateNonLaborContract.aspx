02f2a5abfb31afbb670d8af51733d7ada42b0a78
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-06 00:37:20 +0200

    add form to calculate NonLabor contract

--- SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateNonLaborContract.aspx
+++ SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateNonLaborContract.aspx
@@ -4,4 +4,17 @@
     <div class="jumbotron">
         <h1>NonLabor contract</h1>
     </div>
+    <div class="jumbotron">
+        <asp:Label ID="FirstNameLabel" runat="server" Text="First Name"></asp:Label>
+        <asp:TextBox ID="FirstName" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="MiddleNameLabel" runat="server" Text="Middle Name"></asp:Label>
+        <asp:TextBox ID="MiddleName" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="LastNameLabel" runat="server" Text="Last Name"></asp:Label>
+        <asp:TextBox ID="LastName" runat="server" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="BirthDateLabel" runat="server" Text="Birth Date"></asp:Label>
+        <asp:TextBox ID="BirthDate" runat="server" TextMode="Date" CssClass="form-control"></asp:TextBox>
+        <asp:Label ID="ContractValueLabel" runat="server" Text="Gross Contract Value"></asp:Label>
+        <asp:TextBox ID="ContractValue" runat="server" TextMode="Number" CssClass="form-control"></asp:TextBox>
+        <asp:Button ID="CalculateWage" runat="server" Text="Calculate" CssClass="btn btn-info" />
+    </div>
 </asp:Content>
--- SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateNonLaborContract.aspx.designer.cs
+++ SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateNonLaborContract.aspx.designer.cs
@@ -11,5 +11,104 @@ namespace SalaryCalculator.JobContracts {
     
     
     public partial class CreateNonLaborContract {
+        
+        /// <summary>
+        /// FirstNameLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label FirstNameLabel;
+        
+        /// <summary>
+        /// FirstName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox FirstName;
+        
+        /// <summary>
+        /// MiddleNameLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label MiddleNameLabel;
+        
+        /// <summary>
+        /// MiddleName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MiddleName;
+        
+        /// <summary>
+        /// LastNameLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LastNameLabel;
+        
+        /// <summary>
+        /// LastName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox LastName;
+        
+        /// <summary>
+        /// BirthDateLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label BirthDateLabel;
+        
+        /// <summary>
+        /// BirthDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox BirthDate;
+        
+        /// <summary>
+        /// ContractValueLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ContractValueLabel;
+        
+        /// <summary>
+        /// ContractValue control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ContractValue;
+        
+        /// <summary>
+        /// CalculateWage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button CalculateWage;
     }
 }
