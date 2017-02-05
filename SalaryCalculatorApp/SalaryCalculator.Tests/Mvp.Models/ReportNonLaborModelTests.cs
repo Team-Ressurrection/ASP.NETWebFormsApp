01623707029297d4f5291b590e51f25534c1b0cf
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-06 00:05:33 +0200

    add simple Mvp tests

---
+++ SalaryCalculatorApp/SalaryCalculator.Tests/Mvp.Models/ReportNonLaborModelTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Mvp.Models;
+using SalaryCalculator.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Tests.Mvp.Models
+{
+    [TestFixture]
+    public class ReportNonLaborModelTests
+    {
+        [Test]
+        public void WhenCreatingNewInstance_PropertySetterNonLaborContracts_ShouldReturnIEnumerable()
+        {
+            var model = new ReportNonLaborModel();
+            var mockedBill = new FakeRemunerationBill();
+            var collection = new List<FakeRemunerationBill>() { mockedBill };
+            model.NonLaborContracts = collection;
+
+            Assert.IsInstanceOf<IEnumerable<RemunerationBill>>(model.NonLaborContracts);
+        }
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.Tests/Mvp.Presenters/ReportNonLaborPresenterTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Data.Contracts;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Mvp.Presenters;
+using SalaryCalculator.Mvp.Views;
+using SalaryCalculator.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Tests.Mvp.Presenters
+{
+    [TestFixture]
+    public class ReportNonLaborPresenterTests
+    {
+        [Test]
+        public void Constructor_ShouldThrowArgumentNullException_WhenParameterServiceIsNull()
+        {
+            var view = new Mock<IReportNonLaborView>();
+            var service = new Mock<IRemunerationBillService>();
+
+            Assert.That(() => new ReportNonLaborPresenter(view.Object, null), Throws.InstanceOf<ArgumentNullException>().With.Message.Contains("The argument is null"));
+        }
+
+        [Test]
+        public void Constructor_ShouldCreateInstanceCorrectly_WhenAllParametersArePassed()
+        {
+            var view = new Mock<IReportNonLaborView>();
+            var service = new Mock<IRemunerationBillService>();
+            var repo = new Mock<IRepository<RemunerationBill>>();
+            var bill = new FakeRemunerationBill();
+
+            IReportNonLaborPresenter presenter = new ReportNonLaborPresenter(view.Object, service.Object);
+
+            Assert.IsInstanceOf<IReportNonLaborPresenter>(presenter);
+        }
+    }
+}
