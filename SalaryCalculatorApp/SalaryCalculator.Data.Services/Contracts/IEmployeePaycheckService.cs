28bff24cf2f68e9753b41714b5361eae3e5f492a
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-07 03:00:02 +0200

    add EmployeePaycheckService

---
+++ SalaryCalculatorApp/SalaryCalculator.Data.Services/Contracts/IEmployeePaycheckService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+using SalaryCalculator.Data.Models;
+
+namespace SalaryCalculator.Data.Services.Contracts
+{
+    public interface IEmployeePaycheckService
+    {
+        IQueryable<EmployeePaycheck> GetTop(int count);
+
+        IQueryable<EmployeePaycheck> GetAll();
+
+        EmployeePaycheck GetById(int id);
+
+        void UpdateById(int id, EmployeePaycheck updateBill);
+
+        void DeleteById(int id);
+
+        void Create(EmployeePaycheck bill);
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.Data.Services/EmployeePaycheckService.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+
+using SalaryCalculator.Data.Contracts;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using System;
+using Bytes2you.Validation;
+
+namespace SalaryCalculator.Data.Services
+{
+    public class EmployeePaycheckService : IEmployeePaycheckService
+    {
+        private IRepository<EmployeePaycheck> employeePaychecks;
+
+        public EmployeePaycheckService(IRepository<EmployeePaycheck> employeePaychecks)
+        {
+            this.employeePaychecks = employeePaychecks;
+        }
+
+        public void Create(EmployeePaycheck paycheck)
+        {
+            Guard.WhenArgument(paycheck, "paycheck").IsNull().Throw();
+
+            this.employeePaychecks.Add(paycheck);
+            this.employeePaychecks.SaveChanges();
+        }
+
+        public void DeleteById(int id)
+        {
+            this.employeePaychecks.Delete(id);
+            this.employeePaychecks.SaveChanges();
+        }
+
+        public IQueryable<EmployeePaycheck> GetAll()
+        {
+            return this.employeePaychecks.All;
+        }
+
+        public EmployeePaycheck GetById(int id)
+        {
+            return this.employeePaychecks.GetById(id);
+        }
+
+        public IQueryable<EmployeePaycheck> GetTop(int count)
+        {
+            return this.employeePaychecks.All.Take(count);
+        }
+
+        public void UpdateById(int id, EmployeePaycheck updatePaycheck)
+        {
+            var paycheckToUpdate = this.employeePaychecks.GetById(id);
+
+            paycheckToUpdate.EmployeeId = updatePaycheck.EmployeeId;
+
+            this.employeePaychecks.SaveChanges();
+        }
+    }
+}
