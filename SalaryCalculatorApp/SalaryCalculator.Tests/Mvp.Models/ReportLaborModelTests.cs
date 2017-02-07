18d80e1d911ab549a0ae35efb36cc5c7ad45e826
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-07 03:15:57 +0200

    add tests

---
+++ SalaryCalculatorApp/SalaryCalculator.Tests/Mvp.Models/ReportLaborModelTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Mvp.Models;
+using SalaryCalculator.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Tests.Mvp.Models
+{
+    [TestFixture]
+    public class ReportLaborModelTests
+    {
+        [Test]
+        public void WhenCreatingNewInstance_PropertySetterLaborContracts_ShouldReturnIEnumerable()
+        {
+            var model = new ReportLaborModel();
+            var mockedPaycheck = new FakeEmployeePaycheck();
+            var collection = new List<FakeEmployeePaycheck>() { mockedPaycheck };
+            model.LaborContracts = collection;
+
+            Assert.IsInstanceOf<IEnumerable<FakeEmployeePaycheck>>(model.LaborContracts);
+        }
+
+        [Test]
+        public void WhenPropertyLaborContracts_IsSetProperly_ShouldReturnIEnumerable()
+        {
+            var model = new ReportLaborModel();
+            var mockedPaycheck = new FakeEmployeePaycheck();
+            var collection = new List<FakeEmployeePaycheck>() { mockedPaycheck };
+            model.LaborContracts = collection;
+
+            Assert.AreEqual(collection, model.LaborContracts);
+        }
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.Tests/Mvp.Presenters/ReportLaborPresenterTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using SalaryCalculator.Data.Contracts;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Mvp.Presenters;
+using SalaryCalculator.Mvp.Views;
+using SalaryCalculator.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Tests.Mvp.Presenters
+{
+    [TestFixture]
+    public class ReportLaborPresenterTests
+    {
+        [Test]
+        public void Constructor_ShouldThrowArgumentNullException_WhenParameterServiceIsNull()
+        {
+            var view = new Mock<IReportLaborView>();
+            var service = new Mock<IEmployeePaycheckService>();
+
+            Assert.That(() => new ReportLaborPresenter(view.Object, null), Throws.InstanceOf<ArgumentNullException>().With.Message.Contains("The argument is null"));
+        }
+
+        [Test]
+        [Ignore("Not finished test.")]
+        public void Constructor_ShouldCreateInstanceCorrectly_WhenAllParametersArePassed()
+        {
+            var view = new Mock<IReportLaborView>();
+            var service = new Mock<IEmployeePaycheckService>();
+            var repo = new Mock<IRepository<EmployeePaycheck>>();
+            var bill = new FakeEmployeePaycheck();
+
+            IReportLaborPresenter presenter = new ReportLaborPresenter(view.Object, service.Object);
+
+            Assert.IsInstanceOf<IReportLaborPresenter>(presenter);
+        }
+    }
+}
