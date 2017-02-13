df3114c17e91c7ab5e3710486ee147806cddcee3
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-13 21:14:59 +0200

    add selfEmploymentService

---
+++ SalaryCalculatorApp/SalaryCalculator.Data.Services/Contracts/ISelfEmploymentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+using SalaryCalculator.Data.Models;
+
+namespace SalaryCalculator.Data.Services.Contracts
+{
+    public interface ISelfEmploymentService
+    {
+        IQueryable<SelfEmployment> GetTop(int count);
+
+        IQueryable<SelfEmployment> GetAll();
+
+        SelfEmployment GetById(int id);
+
+        void UpdateById(int id, SelfEmployment updateBill);
+
+        void DeleteById(int id);
+
+        void Create(SelfEmployment bill);
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.Data.Services/SelfEmploymentkService.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+
+using Bytes2you.Validation;
+
+using SalaryCalculator.Data.Contracts;
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Services.Contracts;
+
+namespace SalaryCalculator.Data.Services
+{
+    public class SelfEmploymentService : ISelfEmploymentService
+    {
+        private IRepository<SelfEmployment> selfEmployments;
+
+        public SelfEmploymentService(IRepository<SelfEmployment> selfEmployments)
+        {
+            Guard.WhenArgument(selfEmployments, "SelfEmployment").IsNull().Throw();
+
+            this.selfEmployments = selfEmployments;
+        }
+
+        public void Create(SelfEmployment selfEmpl)
+        {
+            Guard.WhenArgument(selfEmpl, "selfEmpl").IsNull().Throw();
+
+            this.selfEmployments.Add(selfEmpl);
+            this.selfEmployments.SaveChanges();
+        }
+
+        public void DeleteById(int id)
+        {
+            Guard.WhenArgument<int>(id, "id").IsLessThanOrEqual<int>(0).Throw();
+
+            this.selfEmployments.Delete(id);
+            this.selfEmployments.SaveChanges();
+        }
+
+        public IQueryable<SelfEmployment> GetAll()
+        {
+            return this.selfEmployments.All;
+        }
+
+        public SelfEmployment GetById(int id)
+        {
+            Guard.WhenArgument<int>(id, "id").IsLessThanOrEqual<int>(0).Throw();
+
+            return this.selfEmployments.GetById(id);
+        }
+
+        public IQueryable<SelfEmployment> GetTop(int count)
+        {
+            return this.selfEmployments.All.Take(count);
+        }
+
+        public void UpdateById(int id, SelfEmployment updatePaycheck)
+        {
+            this.selfEmployments.Update(updatePaycheck);
+            this.selfEmployments.SaveChanges();
+        }
+    }
+}
