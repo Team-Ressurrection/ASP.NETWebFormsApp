e1ae68317180da0fabbbd9fd1a00a7acd5b3b4a2
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-13 20:40:13 +0200

    add model for creating NonLaborContract

---
+++ SalaryCalculatorApp/SalaryCalculator.Mvp/Models/ICreateNonLaborContractModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using SalaryCalculator.Data.Models;
+
+namespace SalaryCalculator.Mvp.Models
+{
+    public interface ICreateNonLaborContractModel
+    {
+        RemunerationBill RemunerationBill { get; set; }
+    }
+}
