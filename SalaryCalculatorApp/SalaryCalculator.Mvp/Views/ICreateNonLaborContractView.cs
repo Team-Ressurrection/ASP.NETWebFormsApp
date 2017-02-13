67a337e65e0c908fc94f8b77ee1e86621736b0c5
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-13 20:40:57 +0200

    add view for NonLaborContract

---
+++ SalaryCalculatorApp/SalaryCalculator.Mvp/Views/ICreateNonLaborContractView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WebFormsMvp;
+
+using SalaryCalculator.Mvp.EventsArguments;
+using SalaryCalculator.Mvp.Models;
+
+namespace SalaryCalculator.Mvp.Views
+{
+    public interface ICreateNonLaborContractView : IView<CreateNonLaborContractModel>
+    {
+        event EventHandler<RemunerationBillEventArgs> CalculateRemunerationBill;
+
+        event EventHandler<RemunerationBillEventArgs> CreateRemunerationBill;
+    }
+}
