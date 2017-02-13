cdd04850d2c621f96c852ecd5540b68bedf6f830
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-13 20:40:39 +0200

    add presenter for NonLaborContract

---
+++ SalaryCalculatorApp/SalaryCalculator.Mvp/Presenters/CreateNonLaborContractPresenter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Bytes2you.Validation;
+
+using WebFormsMvp;
+
+using SalaryCalculator.Data.Models;
+using SalaryCalculator.Data.Models.Constants;
+using SalaryCalculator.Data.Services.Contracts;
+using SalaryCalculator.Mvp.EventsArguments;
+using SalaryCalculator.Mvp.Views;
+
+namespace SalaryCalculator.Mvp.Presenters
+{
+    public class CreateNonLaborContractPresenter : Presenter<ICreateNonLaborContractView>, ICreateNonLaborContractPresenter
+    {
+        private const decimal PersonalInsurancePercent = 0.188m;
+        private const decimal IncomeTaxPercent = 0.1m;
+
+        private readonly IRemunerationBillService remunerationBillService;
+
+        public CreateNonLaborContractPresenter(ICreateNonLaborContractView view, IRemunerationBillService remunerationBillService)
+            : base(view)
+        {
+            Guard.WhenArgument<IRemunerationBillService>(remunerationBillService, "remunerationBillService").IsNull().Throw();
+
+            this.remunerationBillService = remunerationBillService;
+
+            this.View.CalculateRemunerationBill += CalculateRemunerationBill;
+            this.View.CreateRemunerationBill += CreateRemunerationBill;
+        }
+
+        public void CalculateRemunerationBill(object sender, RemunerationBillEventArgs e)
+        {
+            Guard.WhenArgument<decimal>(e.GrossSalary, "GrossSalary").IsLessThan(0).Throw();
+
+            var remunerationBill = new RemunerationBill();
+            remunerationBill.CreatedDate = DateTime.Now;
+            remunerationBill.EmployeeId = 1;
+            remunerationBill.GrossSalary = e.GrossSalary;
+
+            bool isMaximum = this.CheckMaxSocialSecurityIncome(e.GrossSalary);
+            remunerationBill.SocialSecurityIncome = isMaximum ? ValidationConstants.MaxSocialSecurityIncome : e.GrossSalary;
+
+            remunerationBill.PersonalInsurance = this.GetPersonalInsurance(remunerationBill.SocialSecurityIncome ,PersonalInsurancePercent);
+            remunerationBill.IncomeTax = (remunerationBill.GrossSalary - remunerationBill.PersonalInsurance)*IncomeTaxPercent;
+
+            remunerationBill.NetWage = remunerationBill.GrossSalary - remunerationBill.PersonalInsurance - remunerationBill.IncomeTax;
+
+            this.View.Model.RemunerationBill = remunerationBill;
+        }
+
+        public void CreateRemunerationBill(object sender, RemunerationBillEventArgs e)
+        {
+            Guard.WhenArgument<RemunerationBill>(this.View.Model.RemunerationBill, "RemunerationBill").IsNull().IsNotInstanceOfType(typeof(RemunerationBill)).Throw();
+
+            this.remunerationBillService.Create(this.View.Model.RemunerationBill);
+        }
+        private decimal GetPersonalInsurance(decimal salary, decimal personalInsurancePercent)
+        {
+            return salary * personalInsurancePercent;
+        }
+
+        private bool CheckMaxSocialSecurityIncome(decimal parameter)
+        {
+            var isMaximum = parameter.CompareTo(ValidationConstants.MaxSocialSecurityIncome) >= 0;
+
+            return isMaximum;
+        }
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.Mvp/Presenters/ICreateNonLaborContractPresenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WebFormsMvp;
+
+using SalaryCalculator.Mvp.EventsArguments;
+using SalaryCalculator.Mvp.Views;
+
+namespace SalaryCalculator.Mvp.Presenters
+{
+   public interface ICreateNonLaborContractPresenter : IPresenter<ICreateNonLaborContractView>
+    {
+        void CalculateRemunerationBill(object sender, RemunerationBillEventArgs e);
+
+        void CreateRemunerationBill(object sender, RemunerationBillEventArgs e);
+    }
+}
