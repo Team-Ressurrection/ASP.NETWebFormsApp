5e158d76085b2a6d3cafef6ed209d376761ffb9b
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-13 23:07:11 +0200

    add createFreelance webform

--- SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateFreelanceContract.aspx
+++ SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateFreelanceContract.aspx
@@ -4,4 +4,24 @@
     <div class="jumbotron">
         <h1>Freelance contract</h1>
     </div>
+    <div class="jumbotron">
+        <asp:Label ID="FirstNameLabel" runat="server" Text="First Name" Visible="true"></asp:Label>
+        <asp:TextBox ID="FirstName" runat="server" CssClass="form-control" Visible="true"></asp:TextBox>
+        <asp:Label ID="MiddleNameLabel" runat="server" Text="Middle Name" Visible="true"></asp:Label>
+        <asp:TextBox ID="MiddleName" runat="server" CssClass="form-control" Visible="true"></asp:TextBox>
+        <asp:Label ID="LastNameLabel" runat="server" Text="Last Name" Visible="true"></asp:Label>
+        <asp:TextBox ID="LastName" runat="server" CssClass="form-control" Visible="true"></asp:TextBox>
+        <asp:Label ID="BirthDateLabel" runat="server" Text="Birth Date" Visible="true"></asp:Label>
+        <asp:TextBox ID="BirthDate" runat="server" TextMode="Date" CssClass="form-control" Visible="true"></asp:TextBox>
+        <asp:Label ID="SocialSecurityIncomeLabel" runat="server" Text="Gross Contract Value" Visible="true"></asp:Label>
+        <asp:TextBox ID="SocialSecurityIncome" runat="server" TextMode="Number" CssClass="form-control" Visible="true"></asp:TextBox>
+        <asp:Button ID="CalculateSocialSecurityContributions" runat="server" Text="Calculate" CssClass="btn btn-info" Visible="true" OnClick="CalculateSocialSecurityContributions_Click" />
+    </div>
+    <asp:GridView ID="DetailsViewSelfEmploymentContributions"
+            runat="server"
+            AutoGenerateColumns="true"
+            CssClass="jumbotron">
+            <Columns>
+            </Columns>
+        </asp:GridView>
 </asp:Content>
--- SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateFreelanceContract.aspx.cs
+++ SalaryCalculatorApp/SalaryCalculator/JobContracts/CreateFreelanceContract.aspx.cs
@@ -1,17 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+
+using WebFormsMvp;
+using WebFormsMvp.Web;
+
+using SalaryCalculator.Mvp.EventsArguments;
+using SalaryCalculator.Mvp.Models;
+using SalaryCalculator.Mvp.Presenters;
+using SalaryCalculator.Mvp.Views;
 
 namespace SalaryCalculator.JobContracts
 {
-    public partial class CreateFreelanceContract : System.Web.UI.Page
+    [PresenterBinding(typeof(CreateFreelanceContractPresenter))]
+    public partial class CreateFreelanceContract : MvpPage<CreateFreelanceContractModel>, ICreateFreelanceContractView
     {
+        public event EventHandler<SelfEmploymentEventArgs> CalculateSelfEmployment;
+        public event EventHandler<SelfEmploymentEventArgs> CreateSelfEmployment;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void CalculateSocialSecurityContributions_Click(object sender, EventArgs e)
+        {
+            this.FirstNameLabel.Visible = false;
+            this.FirstName.Visible = false;
+            this.MiddleNameLabel.Visible = false;
+            this.MiddleName.Visible = false;
+            this.LastNameLabel.Visible = false;
+            this.LastName.Visible = false;
+            this.BirthDateLabel.Visible = false;
+            this.BirthDate.Visible = false;
+            this.SocialSecurityIncomeLabel.Visible = false;
+            this.SocialSecurityIncome.Visible = false;
+            this.CalculateSocialSecurityContributions.Visible = false;
+
+
+            var args = new SelfEmploymentEventArgs((decimal.Parse)(this.SocialSecurityIncome.Text), 0, false);
+
+            this.CalculateSelfEmployment?.Invoke(this, args);
+            this.CreateSelfEmployment?.Invoke(this.Model.SelfEmployment, args);
+
+
+            this.DetailsViewSelfEmploymentContributions.DataSource = new[] { this.Model.SelfEmployment };
+            this.DetailsViewSelfEmploymentContributions.DataBind();
+
+            this.DetailsViewSelfEmploymentContributions.Visible = true;
         }
     }
 }
