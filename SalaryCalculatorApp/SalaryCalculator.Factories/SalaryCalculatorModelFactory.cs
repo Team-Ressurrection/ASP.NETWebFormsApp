dce9ab14de9d949323d43f40c593a8f7a5222f4d
Alex Nestorov <nestorov.alexander@gmail.com>
2017-02-17 20:00:48 +0200

    add new factories

---
+++ SalaryCalculatorApp/SalaryCalculator.Factories/SalaryCalculatorModelFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalaryCalculator.Data.Models;
+
+namespace SalaryCalculator.Factories
+{
+    public class SalaryCalculatorModelFactory : ISalaryCalculatorModelFactory
+    {
+        public Employee GetEmployee()
+        {
+            return new Employee();
+        }
+
+        public EmployeePaycheck GetEmployeePaycheck()
+        {
+            return new EmployeePaycheck();
+        }
+
+        public RemunerationBill GetRemunerationBill()
+        {
+            return new RemunerationBill();
+        }
+
+        public SelfEmployment GetSelfEmployment()
+        {
+            return new SelfEmployment();
+        }
+
+        public User GetUser()
+        {
+            return new User();
+        }
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.Mvp/Factories/SalaryCalculatorEventArgsFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalaryCalculator.Mvp.EventsArguments;
+
+namespace SalaryCalculator.Mvp.Factories
+{
+    public class SalaryCalculatorEventArgsFactory : ISalaryCalculatorEventArgsFactory
+    {
+        public IPaycheckEventArgs GetPaycheckEventArgs(decimal grossSalary, decimal fixedBonus, decimal nonFixedBonus)
+        {
+           return new PaycheckEventArgs(grossSalary, fixedBonus, nonFixedBonus);
+        }
+
+        public IRemunerationBillEventArgs GetRemunerationBillEventArgs(decimal grossSalary)
+        {
+            return new RemunerationBillEventArgs(grossSalary);
+        }
+
+        public ISelfEmploymentEventArgs GetSelfEmploymentEventArgs(decimal socialSecurityIncome, decimal additionalSocialSecurityIncome = 0, bool isInsuredForGDM = false)
+        {
+            return new SelfEmploymentEventArgs(socialSecurityIncome, additionalSocialSecurityIncome, isInsuredForGDM);
+        }
+    }
+}
